f54051916afff6f890f68b58ad456f3964c5e3a3
arnold.clarin.dacanay@gmail.com <arnold.clarin.dacanay@gmail.com>
2024-02-01 04:35:10 +0800

    Initial Commit

---
+++ RainfallApi/RainfallApi/Controllers/RainfallController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RainfallApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RainfallController : ControllerBase
+    {
+        public RainfallController()
+        {
+        }
+
+        [HttpGet("id/{stationId}/readings")]
+        public async Task<ActionResult<IActionResult>> GetRainfall(string stationId, [FromQuery] int count = 10)
+        {
+            return Ok();
+        }
+    }
+}
